ea4342e6827245890b240a870645da0bd8bbb2aa
jiwonee02 <nepthun@naver.com>
2025-03-12 21:58:31 +0900

    🎉 [Start] EasePlan 세팅
    2025 졸업프로젝트 'EasePlan' 프론트 세팅

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,30 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: [
+          '.ios.js',
+          '.android.js',
+          '.js',
+          '.jsx',
+          '.ts',
+          '.tsx',
+          '.json',
+        ],
+        alias: {
+          '@assets': './assets',
+          '@components': './src/components',
+          '@screens': './src/screens',
+          '@hooks': './src/hooks',
+          '@services': './src/services',
+          '@navigation': './src/navigation',
+          '@utils': './src/utils',
+          '@store': './src/store',
+        },
+      },
+    ],
+  ],
 };
